25bc698086f102043af7b851b29d54e4241c0b3e
Jomers01 <wearponbest@gmail.com>
2021-07-21 23:16:24 -0500

    Landing Page
    Finalizacion de pagina de aterrizaje freecodecamp

--- FreeCodeCamp/Palindromo/script/script.js
+++ FreeCodeCamp/Palindromo/script/script.js
@@ -2,12 +2,15 @@ let frase;
 let fraseReverse;
 
 
-function frasePalindromo(frase){
+function frasePalindromo(){
     frase = document.querySelector('#frase').value; 
+    console.log(frase)
     fraseReverse = frase;
 
     frase = frase.toLowerCase().replace(/[^a-zA-Z ]/g, "").split("");
+    console.log(frase)
     fraseReverse = fraseReverse.toLowerCase().replace(/[^a-zA-Z ]/g, "").split("").reverse().toString();
+    console.log(fraseReverse)
 
     if (frase == fraseReverse){
         document.querySelector('.result').innerHTML = `Es un palindromo`;
