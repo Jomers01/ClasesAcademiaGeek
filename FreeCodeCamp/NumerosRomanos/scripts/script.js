4663dc66f43c7c05e1efe43486b314352199871c
Jomers01 <wearponbest@gmail.com>
2021-07-23 14:00:41 -0500

    Actualizacion

---
+++ FreeCodeCamp/NumerosRomanos/scripts/script.js
@@ -0,0 +1,19 @@
+//Variables
+let I = 1;
+let V = 5;
+let X = 10;
+let L = 50;
+let C = 100;
+let D = 500;
+let M = 1000;
+let result;
+
+
+function convertidor() {
+    const valor = document.querySelector('#valor').value;
+    console.log(valor);
+
+    if (valor>=1){
+        result = valor
+    }
+}
