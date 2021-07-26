0e27b555a1789559d15188ac375976cd847a7838
Jomers01 <wearponbest@gmail.com>
2021-07-25 23:35:31 -0500

    NetNet v1.0

---
+++ FreeCodeCamp/NetNet/data/data.js
@@ -0,0 +1,107 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Black Widow (2021)",
+        "director": "Cate Shortland",
+        "screenplay": "Eric Pearson",
+        "story": "Ned Benson",
+        "image": "https://www.themoviedb.org/t/p/original/rFBHD0499vOYqH0yHc7R843ErXP.jpg",
+        "clase": 1,
+        "description": "Natasha Romanoff, alias Viuda Negra, se enfrenta a las partes más oscuras de su historia cuando surge una peligrosa conspiración con vínculos con su pasado. Perseguida por una fuerza que no se detendrá ante nada para acabar con ella, Natasha debe enfrentarse a su historia como espía y a las relaciones rotas que dejó a su paso mucho antes de convertirse en una Vengadora."
+    },
+
+    {
+        "id": 2,
+        "title": "Space Jam: A New Legacy (2021)",
+        "director": "Malcolm D. Lee",
+        "writer": "Keenan Coogler",
+        "writer2": "Juel Taylor",
+        "image": "https://www.themoviedb.org/t/p/original/5kUCxgt1oGnNL6WlmLF666R6kwy.jpg",
+        "clase": "img2",
+        "description": "Secuela de la cinta original de 1996, Space Jam, protagonizada por Michael Jordan. En esta segunda parte, la super estrella de la NBA es LeBron James, quien queda atrapado junto a su hijo Dom en un extraño lugar, un espacio digital de una fuerza todopoderosa y malvada conocida como A.I. Para volver a casa y poner a salvo a su hijo, el jugador de baloncesto deberá unir fuerzas con Bugs Bunny, Lola Bunny y el resto de personajes de los Looney Tunes para enfrentarse en un partido de baloncesto a los campeones digitalizados por A.I."
+    },
+
+    {
+        "id": 3,
+        "title": "El Bebé Jefazo 2: Negocios de familia (2021)",
+        "director": "Tom McGrath",
+        "characters": "Michael McCullers",
+        "novel": "Marla Frazee",
+        "image": "https://www.themoviedb.org/t/p/original/85J1DwZowIlKFOF7jllgCD3iHhx.jpg",
+        "clase": 3,
+        "description": "Los hermanos Templeton se han convertido en adultos y se han alejado el uno del otro, pero un nuevo jefe bebé con un enfoque de vanguardia está a punto de unirlos nuevamente e inspirar un nuevo negocio familiar."
+    },
+
+    {
+        "id": 4,
+        "title": "Fast & Furious 9 (2021)",
+        "director": "Justin Lin",
+        "screenplay, story": "Daniel Casey",
+        "characters": "Gary Scott Thompson",
+        "image": "https://www.themoviedb.org/t/p/original/TX1T3c3CDBN0y8iDNvlQbtiA5Z.jpg",
+        "clase": "4",
+        "description": "Dom Toretto lleva una vida tranquila con Letty y su hijo, el pequeño Brian, pero saben que el peligro siempre acecha. Esta vez, esa amenaza obligará a Dom a enfrentarse a los pecados de su pasado si quiere salvar a quienes más quiere. El equipo se vuelve a reunir para impedir un complot a escala mundial, liderado por uno de los asesinos más peligrosos y mejor conductor a los que se han enfrentado; un hombre que además es el hermano desaparecido de Dom, Jakob. Novena entrega de la famosa franquicia."
+    },
+
+    {
+        "id": 5,
+        "title": "Peter Rabbit 2: A la fuga (2021)",
+        "director": "Will Gluck",
+        "writer": "Patrick Burleigh",
+        "image": "https://www.themoviedb.org/t/p/original/gj4Lwldois15IaNnl4tadesHGC4.jpg",
+        "clase": "5",
+        "description": "Bea, Thomas y los conejos han creado una familia improvisada, pero a pesar de sus mejores esfuerzos, Peter no parece sacudirse su traviesa reputación. Aventurándose más allá del ja´dín, peter se encuentra en un mundo que aprecia sus trastadas, pero cuando su familia lo arriesga todo por ir en su búsqueda, Peter deberá decidir qué tipo de conejo quiere ser."
+    },
+
+    {
+        "id": 6,
+        "title": "Un lugar tranquilo 2 (2021)",
+        "director, screenplay": "John Krasinski",
+        "characters": "Bryan Woods",
+        "characters": "Scott Beck",
+        "image": "https://www.themoviedb.org/t/p/original/TX1T3c3CDBN0y8iDNvlQbtiA5Z.jpg",
+        "clase": "6",
+        "description": 'Tras los fatales acontecimientos sucedidos en la primera parte, la familia Abbot debe enfrentarse a los peligros del mundo exterior mientras luchan en silencio por sobrevivir. Forzados a aventurarse en lo desconocido, pronto se dan cuenta de que las criaturas que cazan orientadas por el sonido no son la única amenaza que acecha más allá del camino de arena. Secuela de la exitosa producción "Un lugar tranquilo" (A Quiet Place, 2018).'
+    },
+
+    {
+        "id": 7,
+        "title": "La Purga: Infinita (2021)",
+        "director": "Everardo Gout",
+        "writer": "James DeMonaco",
+        "image": "https://www.themoviedb.org/t/p/original/5yvawJviOjb2nUFvHGXcCpGVTqy.jpg",
+        "clase": "7",
+        "description": 'Para los miembros de un movimiento clandestino, ya no es suficiente una noche anual de anarquía y asesinatos, así que deciden devolver a Estados Unidos el caos y las masacres sin fin, donde nadie volverá a estar a salvo... jamás. Adela y su esposo Juan viven en Texas, donde él trabaja como peón en un rancho para la adinerada familia Tucker. En la mañana después de la tradicional purga, una banda de asesinos enmascarados ataca violenta e ilegalmente a la familia Tucker. Tras este hecho, la familia texana se ve obligada a unirse con Juan y su esposa para enfrentar un país entero a punto de colapsar, que se hunde en un mar de caos y de sangre. Quinta y última entrega de la saga "The Purge".'
+    },
+
+    {
+        "id": 8,
+        "title": "Spirit - Indomable (2021)",
+        "director": "Elaine Bogan",
+        "writer": "John Fusco",
+        "image": "https://www.themoviedb.org/t/p/original/mptEtOkw906BJuD9rRUKG5sr5E9.jpg",
+        "clase": "8",
+        "description": 'La vida de Lucky Prescott cambia para siempre cuando se muda de su casa en la gran ciudad a un pequeño pueblo fronterizo en el que entabla amistad con un caballo salvaje llamado Spirit.'
+    },
+
+    {
+        "id": 9,
+        "title": "Ser padre (2021)",
+        "director": "Paul Weitz",
+        "screenplay": "Dana Stevens",
+        "image": "https://www.themoviedb.org/t/p/original/vqfdiWzcMJjcFzm9YJO29TeH39M.jpg",
+        "clase": "9",
+        "description": 'Un viudo convertido en papá lidia con las dudas, los miedos, el dolor y los pañales sucios mientras trata de criar a su hija solo. Inspirada en una historia real.'
+    },
+
+    {
+        "id": 10,
+        "title": "Snake Eyes (2021)",
+        "director": "Robert Schwentke",
+        "screenplay": "Evan Spiliotopoulos",
+        "screenplay": "Joe Shrapnel",
+        "image": "https://www.themoviedb.org/t/p/original/oXyiy9YL9AUkElqhtb6V8MLfnkt.jpg",
+        "clase": "10",
+        "description": 'Después de salvar la vida de su heredero aparente, el tenaz y solitario Snake Eyes es bienvenido en un antiguo clan japonés llamado Arashikage, donde se le enseña los caminos del guerrero ninja. Pero, cuando se revelen secretos de su pasado, se pondrá a prueba el honor y la lealtad de Snake Eyes, incluso si eso significa perder la confianza de los más cercanos a él.'
+    },
+]
