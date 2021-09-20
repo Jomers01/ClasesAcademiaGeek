efaea85ff852abb690d30f18eb2e2702a79ff40e
Jomers01 <wearponbest@gmail.com>
2021-09-20 15:39:00 -0500

    Calculadora

---
+++ Retos_FrontEnd4/calculadora/src/Components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Display.css";
+
+const Display = ({view}) => {
+    return(
+        <div className="Display">
+            {view}
+        </div>
+    );
+}
+
+export default Display
